c9633f03fa3796afe9b62862732f1c3b89e92910
Tiago Amaro <tiagopadrela@gmail.com>
2020-11-17 19:31:18 -0300

    Inicio da aula, antes da solucao do exercicio 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FaleConoscoComponent } from './sobre/fale-conosco/fale-conosco.component';
+import { FaqComponent } from './sobre/faq/faq.component';
+import { HistoriaComponent } from './sobre/historia/historia.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent,
+  }, {
+    path: '', redirectTo: '/home', pathMatch: 'full'
+  },
+  {
+    path: 'sobre', component: SobreComponent, children: [
+      {
+        path: 'faq', component: FaqComponent,
+      },
+      {
+        path: 'historia', component: HistoriaComponent,
+      },
+      {
+        path: 'fale-conosco', component: FaleConoscoComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { FaqComponent } from './sobre/faq/faq.component';
+import { HistoriaComponent } from './sobre/historia/historia.component';
+import { FaleConoscoComponent } from './sobre/fale-conosco/fale-conosco.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobreComponent,
+    FaqComponent,
+    HistoriaComponent,
+    FaleConoscoComponent
   ],
   imports: [
     BrowserModule,
